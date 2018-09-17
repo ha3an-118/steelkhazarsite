ba06626d28f52c7c25d1387346b3e437fe5b0d4b
habedi <info@habedi.ir>
2018-09-17 09:46:04 +0430

    write js functions that can show image in slider and next and prev img and can hide all img

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,74 @@
+//this variable for what pic is show in each slider
+var picShowCont=[];
 $(document).ready(function(){
-    var slideElm=$('[slide="slider"]');
-    var i=0;
-    var j=0;
-    var imageArray;
-    for(i=0;i<slideElm.length;i++) {
-        imageArray=$(slideElm[i]).find("[tag='image'] img");
-        for(j=0;j<imageArray.length;j++){
-            $(imageArray[j]).hide();
-            }
-        $(slideElm[i]).find("[tag='image'] img.show").show();
+    var sliders=$('[role="slider"]');
+    var sliderImgs; //for get all images is on my slider
+    var tempPush; //this is template var for push data in picShowCont
+  //part one hide all pic in sliders
+   var i,j;
+   for(i=0;i<sliders.length;i++){
+        sliderImgs=$(sliders[i]).find("[tag='image'] img");
+        for(j=0;j<sliderImgs.length;j++){
+            $(sliderImgs[j]).hide();
+        }
+        tempPush={id:$(sliders[i]).attr('id') , imgShow:0};
+        picShowCont.push(tempPush);
+        showImg($(sliders[i]).attr('id'),0);
     }
-    var sliderName;
-    var slideNum;
-    var temp;
-    $(slideElm).find("li").click(function(){
-        sliderName=$(this).attr("targ");
-        slideNum=$(this).attr("targ-img");
-        temp= $(sliderName).find("[tag='image'] img ");
-        for(i=0;i<temp.length;i++){
-            $(temp[i]).hide();
-            }
-        $(temp[slideNum]).show();
-        });
+   $(sliders).find("li").click(function(){
+       var sliderId=$(this).attr("targ");
+       var imgNum=$(this).attr("targ-img");
+       showImg(sliderId , imgNum);
+       
+       });
+   $(sliders).find("[tag='control'] div").click(function(){
+       var slidId=$(this).attr('targ');
+       var slidAct=$(this).attr('act');
+       switch(slidAct){
+           case 'next':
+                showNextImg(slidId);
+                break;
+            case 'prev':
+                showPrevImg(slidId);
+                break;
+           
+           }
+       });
 });
+function showImg(sliderId ,imgNum ){
+    var findIndex=findImgIndex(sliderId);
+   var imgs=$("#"+sliderId).find("[tag='image'] img");
+    $(imgs[picShowCont[findIndex].imgShow]).fadeOut(10);
+    picShowCont[findIndex].imgShow=imgNum;
+    $(imgs[imgNum]).fadeIn(100);
+}
+function showNextImg(sliderId){
+    var findIndex=findImgIndex(sliderId);
+    var imgNum=picShowCont[findIndex].imgShow;
+    var numOfImg=$("#"+sliderId).find("[tag='image'] img").length;
+    if(++imgNum<numOfImg)
+        showImg(sliderId,imgNum);
+    else
+        showImg(sliderId,0);
+    }
+function findImgIndex(sliderId){
+    var i=0;
+    var findIndex;
+    for(i=0;i<picShowCont.length;i++){
+        if(picShowCont[i].id==sliderId){
+            return i;
+        }
+    }
+}
+function showPrevImg(sliderId){
+    var findIndex=findImgIndex(sliderId);
+    var imgNum=picShowCont[findIndex].imgShow;
+    var numOfImg=$("#"+sliderId).find("[tag='image'] img").length;
+    if(--imgNum>=0)
+        showImg(sliderId,imgNum);
+    else
+        showImg(sliderId,numOfImg-1);
+    }
+f
+
 
